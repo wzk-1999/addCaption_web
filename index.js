7cbb43400f25eea87e1d8bf7bcc75559b14dada8
Zhankui Wang <tlisa2053@gmail.com>
2024-02-18 19:29:14 -0500

    20240218_wzk

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ function closeThisFunction() {
 function switchBtn() {
   if (switchButton.checked) {
     addYourSubtitle();
-    console.log("addYourSubtitle");
+    // console.log("addYourSubtitle");
   } else {
     closeThisFunction();
     console.log("closeThisFunction");
@@ -181,7 +181,7 @@ function addYourSubtitle() {
 
   subtitle.input = document.createElement("input");
   subtitle.input.placeholder = "Type customized subtitle";
-  subtitle.input.type = "text";
+  subtitle.input.type = "search";
   subtitle.input.id = "inputCaption";
   subtitle.input.setAttribute(
     "style",
@@ -202,24 +202,42 @@ function addYourSubtitle() {
     "position:relative;margin-top:3.5rem;margin-left:3rem;color:red;background-color:Aquamarine;border-radius: 12px;"
   );
 
-  subtitle.deleteCurrent = document.createElement("button");
-  subtitle.deleteCurrent.innerText = "×";
-  subtitle.deleteCurrent.setAttribute("class", "close");
-  subtitle.deleteCurrent.setAttribute("data-dismiss", "modal");
-  subtitle.deleteCurrent.setAttribute("aria-hidden", "true");
-  subtitle.deleteCurrent.setAttribute(
+  // why I can't set the clear small button in the right of the input box?
+  // just use the search input type instead
+
+  //   subtitle.deleteCurrent = document.createElement("button");
+  //   // subtitle.deleteCurrent.innerText = "×";
+  //   subtitle.deleteCurrent.innerHtml = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+  //   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
+  // </svg>`;
+  //   subtitle.deleteCurrent.setAttribute("class", "close");
+  //   subtitle.deleteCurrent.setAttribute("data-dismiss", "modal");
+  //   subtitle.deleteCurrent.setAttribute("aria-hidden", "true");
+  //   subtitle.deleteCurrent.setAttribute(
+  //     "style",
+  //     "right:2px;top:-2px;cursor:pointer;display:none;"
+  //   );
+
+  subtitle.completeOrTemporarySave = document.createElement("button");
+  subtitle.completeOrTemporarySave.innerText = "save";
+  subtitle.completeOrTemporarySave.setAttribute(
     "style",
-    "right:2px;top:-2px;cursor:pointer;display:none;"
+    "position:relative;margin-top:3.5rem;margin-left:3rem;color:black;background-color:azure;border-radius: 12px;"
   );
 
   document.body.appendChild(subtitle.inputoverall);
   subtitle.inputoverall.appendChild(subtitle.input);
-  subtitle.inputoverall.appendChild(subtitle.deleteCurrent);
+  // subtitle.inputoverall.appendChild(subtitle.deleteCurrent);
   document.body.appendChild(subtitle.submit);
   document.body.appendChild(subtitle.giveUp);
+  document.body.appendChild(subtitle.completeOrTemporarySave);
 
   subtitle.giveUp.addEventListener("click", confirmAndGiveup);
   subtitle.submit.addEventListener("click", submitAndStore);
+  subtitle.completeOrTemporarySave.addEventListener(
+    "click",
+    completeOrTempSave
+  );
 
   // console.log(typeof subtitle); object
   // console.log(subtitle);
@@ -262,59 +280,242 @@ function confirmAndGiveup() {
   }
 }
 
+function completeOrTempSave() {
+  // console.log("completeOrTempSave has go");
+
+  if (localStorage.getItem("caption") === null) {
+    alert("you haven't submit any captions, save is senseless");
+    return false;
+  }
+
+  // judge it's first complete (need add WEBVTT prefix) or save at the previous save basis (do not need add prefix)
+
+  if (localStorage.getItem("caption").substring(0, 6) != "WEBVTT") {
+    localStorage.setItem(
+      "caption",
+      "WEBVTT\r\n" + localStorage.getItem("caption")
+    );
+  }
+
+  saveFunction();
+}
+
+function saveFunction() {
+  // console.log("saveFunction has go");
+
+  //remove the subtitle edit elements that add just now
+
+  Object.entries(subtitle).map((entry) => {
+    let value = entry[1];
+
+    value.remove();
+  });
+
+  let videoOriginalName = window.location.pathname.split("/").pop();
+  videoOriginalName = videoOriginalName.substring(
+    0,
+    videoOriginalName.lastIndexOf(".")
+  );
+  let curDate = new Date();
+
+  var fileSuffix =
+    "_" +
+    curDate.toLocaleDateString("es-CL") +
+    "_" +
+    curDate.toTimeString().substring(0, 8);
+
+  videoOriginalName = videoOriginalName + fileSuffix;
+
+  let vttFileName = "caption" + fileSuffix;
+
+  // add the vtt file quote
+
+  let vttQuote = document.createElement("track");
+  vttQuote.setAttribute("label", "English");
+  vttQuote.setAttribute("kind", "subtitles");
+  vttQuote.setAttribute("srclang", "en");
+
+  vttQuote.setAttribute("src", `${vttFileName}.vtt`);
+
+  vttQuote;
+  vttQuote.default = true;
+
+  document.getElementsByTagName("video")[0].appendChild(vttQuote);
+
+  localStorage.getItem("caption");
+
+  var htmlContent = document.body.innerHTML;
+  var bl = new Blob([htmlContent], { type: "text/html" });
+  var a = document.createElement("a");
+
+  //how to add address a url contains chinese characters like "朴树%20-%20清白之年%20(Live特别版)_哔哩哔哩_bilibili.mp4" ?
+  // which will cause garbled characters
+
+  // console.log(videoOriginalName);
+  a.href = URL.createObjectURL(bl);
+  a.download = `${videoOriginalName}.html`;
+  a.hidden = true;
+  document.body.appendChild(a);
+  a.innerHTML = "completeOrTempSave";
+  a.click();
+
+  // save the .vtt file
+  // chrome.downloads api can't be used in content-script
+  // chrome.downloads.download(
+  //   {
+  //     url: "data:text/vtt," + localStorage.getItem("caption"),
+  //     filename: "data.vtt",
+  //     conflictAction: "uniquify", // or "overwrite" / "prompt"
+  //     saveAs: false, // true gives save-as dialogue
+  //   },
+  //   function (downloadId) {
+  //     console.log("Downloaded item with ID", downloadId);
+  //   }
+  // );
+
+  // can't use download vtt file? error:
+  // Unsafe attempt to load URL file:///C:/add_caption/subtitle.vtt from frame with URL
+  // file:///C:/add_caption/add_caption.html. 'file:' URLs are treated as unique security origins.
+
+  bl = new Blob([localStorage.getItem("caption")], { type: "text/vtt" });
+
+  // let videoOriginalName = window.location.pathname.split("/").pop();
+  // videoOriginalName = videoOriginalName.substring(
+  //   0,
+  //   videoOriginalName.lastIndexOf(".")
+  // );
+  // let curDate = new Date();
+
+  // videoOriginalName =
+  //   videoOriginalName +
+  //   "_" +
+  //   curDate.toLocaleDateString() +
+  //   "_" +
+  //   curDate.toTimeString().substring(0, 8);
+  // console.log(videoOriginalName);
+  a.href = URL.createObjectURL(bl);
+
+  a.download = `${vttFileName}.vtt`;
+
+  a.innerHTML = "saveVttFile";
+  // a.target = "_blank";
+  a.click();
+
+  localStorage.removeItem("caption");
+}
+
 function appendToStorage(name, data) {
   var old = localStorage.getItem(name);
   if (old === null) old = "";
   localStorage.setItem(name, old + data);
 }
 
+var timeArray = [];
+
 function submitAndStore() {
-  adjustBeginAndEndTime();
-  console.log("submitAndStore has go");
-  let caption = document.getElementById("inputCaption").value;
-  // localStorage.setItem("caption", caption);
-  appendToStorage("caption", caption);
+  // only start time and end time both been set , then can add the caption content and the "-->" mark
+
+  // set begin time and end time in to time array
+  adjustBeginAndEndtime();
+  if (timeArray[0] != null && timeArray[1] != null) {
+    appendToStorage("caption", "\r\n");
+    appendToStorage("caption", timeArray[0]);
+    appendToStorage("caption", " --> ");
+    appendToStorage("caption", timeArray[1]);
+    appendToStorage("caption", "\r\n");
+
+    let caption = document.getElementById("inputCaption").value;
+    // localStorage.setItem("caption", caption);
+    appendToStorage("caption", caption);
+    appendToStorage("caption", "\r\n");
+  }
+
+  // console.log("submitAndStore has go");
 }
 
-function adjustBeginAndEndTime() {
-  // let startTime = document.createElement("input");
-  // startTime.setAttribute("type", "time");
-  // startTime.setAttribute("id", "startTime");
-  // startTime.setAttribute("step", ".01");
+function adjustBeginAndEndtime() {
+  // let timeArray[0] = document.createElement("input");
+  // timeArray[0].setAttribute("type", "time");
+  // timeArray[0].setAttribute("id", "timeArray[0]");
+  // timeArray[0].setAttribute("step", ".01");
 
-  // startTime.setAttribute("style", "position:relative;margin-top:3rem;");
+  // timeArray[0].setAttribute("style", "position:relative;margin-top:3rem;");
 
-  // let endTime = document.createElement("input");
-  // endTime.setAttribute("type", "time");
-  // endTime.setAttribute("id", "endTime");
-  // endTime.setAttribute("step", ".01");
+  // let timeArray[1] = document.createElement("input");
+  // timeArray[1].setAttribute("type", "time");
+  // timeArray[1].setAttribute("id", "timeArray[1]");
+  // timeArray[1].setAttribute("step", ".01");
 
-  // endTime.setAttribute("style", "position:relative;margin-top:3rem;");
+  // timeArray[1].setAttribute("style", "position:relative;margin-top:3rem;");
 
-  // document.body.appendChild(startTime);
-  // document.body.appendChild(endTime);
+  // document.body.appendChild(timeArray[0]);
+  // document.body.appendChild(timeArray[1]);
 
   var currentVideo = document.getElementsByTagName("video")[0];
 
-  let startTime = prompt(
+  timeArray[0] = prompt(
     "Please enter start time:\n (format:mi:ss.sss)",
-    currentVideo.currentTime - 2
+    convertTimestamp(currentVideo.currentTime - 2)
   );
-  currentVideo.currentTime = currentTime - 2;
-  currentVideo.pause();
-  if (startTime != null) {
-    appendToStorage("caption", startTime);
-  }
 
-  let endTime = prompt(
+  // console.log(currentVideo.currentTime);
+  // console.log(currentVideo.currentTime - 2);
+
+  // record the currentTime :
+  var submitTime = currentVideo.currentTime;
+
+  currentVideo.currentTime = currentVideo.currentTime - 2;
+  currentVideo.pause();
+
+  // appendToStorage("caption", "\r\n");
+
+  // if (timeArray[0] != null) {
+  //   appendToStorage("caption", timeArray[0]);
+  // }
+
+  timeArray[1] = prompt(
     "Please enter end time:\n (format:mi:ss.sss)",
-    currentVideo.currentTime
+    convertTimestamp(submitTime)
   );
 
-  currentVideo.currentTime = currentTime;
+  currentVideo.currentTime = submitTime;
   currentVideo.pause();
 
-  if (endTime != null) {
-    appendToStorage("caption", endTime);
+  // appendToStorage("caption", " --> ");
+
+  // if (timeArray[1] != null) {
+  //   appendToStorage("caption", timeArray[1]);
+  // }
+
+  // appendToStorage("caption", "\r\n");
+  // after confirm the start and end time of subtitle, start the video again
+  currentVideo.play();
+}
+
+// convert the decimal timestamp to hh:mm:ss.sss format
+function convertTimestamp(num) {
+  var hours = Math.floor(num / 3600);
+  var minutes = Math.floor((num - 3600 * hours) / 60);
+  var seconds = Math.floor(num - 3600 * hours - minutes * 60);
+  var milliseconds = Math.floor(
+    (num - 3600 * hours - minutes * 60 - seconds) * 1000
+  );
+
+  if (hours < 10) {
+    hours = "0" + hours;
   }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  if (milliseconds < 100 && milliseconds > 9) {
+    milliseconds = "0" + milliseconds;
+  }
+  if (milliseconds < 10) {
+    milliseconds = "00" + milliseconds;
+  }
+
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
 }
